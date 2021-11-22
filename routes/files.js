1f7db1b48379d8a67199bdefedc6ec60f0c8f7a7
Divesh Jain <dj12@iitbbs.ac.in>
2021-11-22 06:06:41 +0530

    Add Scheduler

--- routes/files.js
+++ routes/files.js
@@ -15,7 +15,7 @@ let storage = multer.diskStorage({
 });
 
 let upload = multer({ storage, limits: { fileSize: 1000000 * 100 } }).single(
-  "myFile"
+  "myfile"
 ); //100mb
 
 router.post("/", (req, res) => {
@@ -35,36 +35,44 @@ router.post("/", (req, res) => {
 });
 
 router.post("/send", async (req, res) => {
-  //Validate request
-  const { uuid, emailTo, emailFrom } = req.body;
+  const { uuid, emailTo, emailFrom, expiresIn } = req.body;
   if (!uuid || !emailTo || !emailFrom) {
-    return res.status(422).send({ error: "All fields are required" });
+    return res
+      .status(422)
+      .send({ error: "All fields are required except expiry." });
   }
-
-  //Get data from database
-  const file = await File.findOne({ uuid: uuid });
-  if (file.sender === emailFrom) {
-    return res.status(422).send({ error: "Email already sent!" });
+  // Get data from db
+  try {
+    const file = await File.findOne({ uuid: uuid });
+    if (file.sender) {
+      return res.status(422).send({ error: "Email already sent once." });
+    }
+    file.sender = emailFrom;
+    file.receiver = emailTo;
+    const response = await file.save();
+    // send mail
+    const sendMail = require("../services/mailService");
+    sendMail({
+      from: emailFrom,
+      to: emailTo,
+      subject: "inShare file sharing",
+      text: `${emailFrom} shared a file with you.`,
+      html: require("../services/emailTemplate")({
+        emailFrom,
+        downloadLink: `${process.env.APP_BASE_URL}/files/${file.uuid}?source=email`,
+        size: parseInt(file.size / 1000) + " KB",
+        expires: "24 hours",
+      }),
+    })
+      .then(() => {
+        return res.json({ success: true });
+      })
+      .catch((err) => {
+        return res.status(500).json({ error: "Error in email sending." });
+      });
+  } catch (err) {
+    return res.status(500).send({ error: "Something went wrong." });
   }
-  file.sender = emailFrom;
-  file.receiver = emailTo;
-  const response = await file.save();
-
-  //sending the email
-  const sendMail = require("../services/emailService.js");
-  sendMail({
-    from: emailFrom,
-    to: emailTo,
-    subject: "Received a file through FileBhejo.",
-    text: `${emailFrom} shared a file with you.`,
-    html: require("../services/emailTemplate")({
-      emailFrom: emailFrom,
-      downloadLink: `${process.env.APP_BASE_URL}/files/${file.uuid}`,
-      size: parseInt(file.size / 1000) + "KB",
-      expires: "24 hours",
-    }),
-  });
-  return res.send({ success: "True" });
 });
 
 module.exports = router;
